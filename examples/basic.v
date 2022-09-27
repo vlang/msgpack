77b3de432fa1ce81bae5fe7df7e538675e9d65af
Joe Conigliaro <joe@vlang.io>
2022-09-27 22:08:37 +1000

    update examples/basic.v

--- examples/basic.v
+++ examples/basic.v
@@ -8,9 +8,10 @@ pub struct TestStructA {
 	field_b string         [codec: 'codecdata2']
 	field_c TestStructB
 	field_d []string
+	// TODO: fix compiler (comptime generic infer)
 	// field_e map[string]int
 	// field_f map[string]TestStructB
-	// field_g []TestStructB
+	field_g []TestStructB
 	field_h time.Time
 }
 
@@ -44,33 +45,42 @@ fn main() {
 		// 		field_b: 'field_f.b.TestStructB.field_b'
 		// 	}
 		// }
-		// field_g: [
-		// 	TestStructB{
-		// 		field_a: 1
-		// 		field_b: 'field_g.b.TestStructB.field_b'
-		// 	},
-		// 	TestStructB{
-		// 		field_a: 2
-		// 		field_b: 'field_g.b.TestStructB.field_b'
-		// 	}
-		// ]
+		field_g: [
+			TestStructB{
+				field_a: 1
+				field_b: 'field_g.b.TestStructB.field_b'
+			},
+			TestStructB{
+				field_a: 2
+				field_b: 'field_g.b.TestStructB.field_b'
+			}
+		]
 		field_h: time.now()
 	}
-	_ = ts
+	
+	// encode data
 	// mut encoder := msgpack.new_encoder()
 	// encoded := encoder.encode<TestStructA>(ts)
-	
 	encoded := msgpack.encode<TestStructA>(ts)
+	println('ts encoded:')
+	println(encoded.hex())
+	
 	// encoded := msgpack.encode<int>(111)
 	// encoded := msgpack.encode('msgpack vlang')
 	// encoded := msgpack.encode({'a': 1, 'b': 2, 'c': 3, 'd': 4})
 	// encoded := msgpack.encode(['apple', 'banana'])
-	println(encoded.hex())
+	
 
+	// decode bytes
+	println('ts decoded:')
 	mut decoder := msgpack.new_decoder()
-	// decoder.decode(encoder.b)
-	decoder.decode_from_string('81ab546573745374727563744184a76669656c645f61d20000006fa76669656c645f62b354657374537472756374412e6669656c645f62a76669656c645f6381ab546573745374727563744282a76669656c645f61d2000000dea76669656c645f62b354657374537472756374422e6669656c645f62a76669656c645f64d6ff631396ef') or {
+	decoder.decode(encoded) or {
 		panic('error decoding: $err')
 	}
-	// decoder.decode_string('92a56170706c65a762616e616e6e61')
+
+	// decode string
+	// decoder.decode(encoder.b)
+	// decoder.decode_from_string('81ab546573745374727563744184a76669656c645f61d20000006fa76669656c645f62b354657374537472756374412e6669656c645f62a76669656c645f6381ab546573745374727563744282a76669656c645f61d2000000dea76669656c645f62b354657374537472756374422e6669656c645f62a76669656c645f64d6ff631396ef') or {
+	// 	panic('error decoding: $err')
+	// }
 }
